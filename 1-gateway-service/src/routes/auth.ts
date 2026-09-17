de9dcf46ef890d65ab482d68b0464679a612d5f7
Mayank Sharma <mayankece155@gmail.com>
2026-09-17 11:52:41 +0530

    feat(auth): add authentication routes , currentuser, refreshtoken , forgotpassword, resetpassword and email verification endpoints

--- 1-gateway-service/src/routes/auth.ts
+++ 1-gateway-service/src/routes/auth.ts
@@ -1,9 +1,9 @@
-// import { Password } from '@gateway/controllers/auth/password';
+import { Password } from '@gateway/controllers/auth/password';
 // import { AuthSeed } from '@gateway/controllers/auth/seed';
 import { SignIn } from '@gateway/controllers/auth/signin';
 // import { Signout } from '@gateway/controllers/auth/signout';
 import { SignUp } from '@gateway/controllers/auth/signup';
-// import { VerifyEmail } from '@gateway/controllers/auth/verify-email';
+import { VerifyEmail } from '@gateway/controllers/auth/verify-email';
 // import { VerifyOTP } from '@gateway/controllers/auth/verify-otp';
 import express, { Router } from 'express';
 
@@ -18,11 +18,11 @@ class AuthRoutes {
     this.router.post('/auth/signup', SignUp.prototype.create);
     this.router.post('/auth/signin', SignIn.prototype.read);
     // this.router.post('/auth/signout', Signout.prototype.update);
-    // this.router.put('/auth/verify-email', VerifyEmail.prototype.update);
+    this.router.put('/auth/verify-email', VerifyEmail.prototype.update);
     // this.router.put('/auth/verify-otp/:otp', VerifyOTP.prototype.update);
-    // this.router.put('/auth/forgot-password', Password.prototype.forgotPassword);
-    // this.router.put('/auth/reset-password/:token', Password.prototype.resetPassword);
-    // this.router.put('/auth/change-password', Password.prototype.changePassword);
+    this.router.put('/auth/forgot-password', Password.prototype.forgotPassword);
+    this.router.put('/auth/reset-password/:token', Password.prototype.resetPassword);
+    this.router.put('/auth/change-password', Password.prototype.changePassword);
     // this.router.put('/auth/seed/:count', AuthSeed.prototype.create);
     return this.router;
   }
--- 3-auth-service/src/routes/auth.ts
+++ 3-auth-service/src/routes/auth.ts
@@ -1,7 +1,7 @@
-// import { changePassword, forgotPassword, resetPassword } from '@auth/controllers/password';
+import { changePassword, forgotPassword, resetPassword } from '@auth/controllers/password';
 import { read } from '@auth/controllers/signin';
 import { create } from '@auth/controllers/signup';
-// import { update } from '@auth/controllers/verify-email';
+import { update } from '@auth/controllers/verify-email';
 // import { updateOTP } from '@auth/controllers/verify-otp';
 import express, { Router } from 'express';
 
@@ -10,11 +10,11 @@ const router: Router = express.Router();
 export function authRoutes(): Router {
   router.post('/signup', create);
   router.post('/signin', read);
-  //   router.put('/verify-email', update);
+  router.put('/verify-email', update);
   //   router.put('/verify-otp/:otp', updateOTP);
-  //   router.put('/forgot-password', forgotPassword);
-  //   router.put('/reset-password/:token', resetPassword);
-  //   router.put('/change-password', changePassword);
+  router.put('/forgot-password', forgotPassword);
+  router.put('/reset-password/:token', resetPassword);
+  router.put('/change-password', changePassword);
 
   return router;
 }
