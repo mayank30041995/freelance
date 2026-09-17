f6c8773c42e7144113509f626190c68ab6c8950e
Mayank Sharma <mayankece155@gmail.com>
2026-09-17 10:19:06 +0530

    auth and gateway signin routes testing

--- 1-gateway-service/src/routes/auth.ts
+++ 1-gateway-service/src/routes/auth.ts
@@ -1,6 +1,6 @@
 // import { Password } from '@gateway/controllers/auth/password';
 // import { AuthSeed } from '@gateway/controllers/auth/seed';
-// import { SignIn } from '@gateway/controllers/auth/signin';
+import { SignIn } from '@gateway/controllers/auth/signin';
 // import { Signout } from '@gateway/controllers/auth/signout';
 import { SignUp } from '@gateway/controllers/auth/signup';
 // import { VerifyEmail } from '@gateway/controllers/auth/verify-email';
@@ -16,7 +16,7 @@ class AuthRoutes {
 
   public routes(): Router {
     this.router.post('/auth/signup', SignUp.prototype.create);
-    // this.router.post('/auth/signin', SignIn.prototype.read);
+    this.router.post('/auth/signin', SignIn.prototype.read);
     // this.router.post('/auth/signout', Signout.prototype.update);
     // this.router.put('/auth/verify-email', VerifyEmail.prototype.update);
     // this.router.put('/auth/verify-otp/:otp', VerifyOTP.prototype.update);
