916534649b25a2255dd05b3f4142a2b7e8548c14
Mayank Sharma <mayankece155@gmail.com>
2026-09-18 15:44:46 +0530

    Users service setup and installation

---
+++ 4-users-service/src/elasticsearch.ts
@@ -0,0 +1,31 @@
+import { Client } from '@elastic/elasticsearch';
+import { config } from '@users/config';
+import { winstonLogger } from '@mayank30041995/jobber-shared';
+import { Logger } from 'winston';
+
+const log: Logger = winstonLogger(`${config.ELASTIC_SEARCH_URL}`, 'usersElasticSearchServer', 'debug');
+
+const elasticSearchClient = new Client({
+  node: config.ELASTIC_SEARCH_URL,
+  auth: {
+    username: config.ELASTIC_SEARCH_USERNAME!,
+    password: config.ELASTIC_SEARCH_PASSWORD!
+  }
+});
+
+export async function checkConnection(): Promise<void> {
+  let isConnected = false;
+
+  while (!isConnected) {
+    try {
+      const health = await elasticSearchClient.cluster.health();
+
+      log.info(`UsersService Elasticsearch health status - ${health.status}`);
+
+      isConnected = true;
+    } catch (error) {
+      log.error('Connection to Elasticsearch failed. Retrying...');
+      log.log('error', 'UsersService checkConnection() method:', error);
+    }
+  }
+}
