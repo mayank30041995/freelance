0188729b349e526de5360c7a20aece5557b2cb19
Mayank Sharma <mayankece155@gmail.com>
2026-09-18 11:33:49 +0530

    Auth Seed Http test

--- 1-gateway-service/src/routes/auth.ts
+++ 1-gateway-service/src/routes/auth.ts
@@ -1,5 +1,5 @@
 import { Password } from '@gateway/controllers/auth/password';
-// import { AuthSeed } from '@gateway/controllers/auth/seed';
+import { AuthSeed } from '@gateway/controllers/auth/seed';
 import { SignIn } from '@gateway/controllers/auth/signin';
 // import { Signout } from '@gateway/controllers/auth/signout';
 import { SignUp } from '@gateway/controllers/auth/signup';
@@ -23,7 +23,7 @@ class AuthRoutes {
     this.router.put('/auth/forgot-password', Password.prototype.forgotPassword);
     this.router.put('/auth/reset-password/:token', Password.prototype.resetPassword);
     this.router.put('/auth/change-password', Password.prototype.changePassword);
-    // this.router.put('/auth/seed/:count', AuthSeed.prototype.create);
+    this.router.put('/auth/seed/:count', AuthSeed.prototype.create);
     return this.router;
   }
 }
